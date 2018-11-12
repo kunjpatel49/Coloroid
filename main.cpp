1b0487d460536faa1a405a21f166e0db79aaaccc
Kunj Patel <37160487+kunjpatel49@users.noreply.github.com>
2018-11-11 20:37:03 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Program 4 Graphics
+//
+//  Created by Kunj Patel on 10/22/18.
+//  Copyright © 2018 Kunj Patel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
